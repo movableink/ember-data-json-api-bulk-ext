39b5e139c809811a18572727163b3eb3f5ae7eaf
Alex LaFroscia <alex@lafroscia.com>
2020-03-20 10:43:31 -0400

    test: put creation tests in module

--- tests/unit/with-bulk-actions-test.js
+++ tests/unit/with-bulk-actions-test.js
@@ -15,78 +15,80 @@ module('Unit | withBulkActions', function(hooks) {
     this.server.post('/posts', this.postsHandler);
   });
 
-  test('it does not interfere with normal creation', async function(assert) {
-    td.when(
-      this.postsHandler(
-        payloadMatches({ data: { type: 'posts', attributes: { title: 'First Post' } } })
-      )
-    ).thenReturn([
-      201,
-      {},
-      JSON.stringify({
-        data: {
-          type: 'posts',
-          id: 1,
-          attributes: {
-            title: 'First Post'
-          }
-        }
-      })
-    ]);
-
-    const first = this.store.createRecord('post', { title: 'First Post' });
-
-    await first.save();
-
-    assert.equal(first.id, 1, 'Recieved an ID from the API');
-  });
-
-  test('it can create multiple models at once', async function(assert) {
-    td.when(
-      this.postsHandler(
-        payloadMatches({
-          data: [
-            { type: 'posts', attributes: { title: 'First Post' } },
-            { type: 'posts', attributes: { title: 'Second Post' } }
-          ]
-        })
-      )
-    ).thenReturn([
-      201,
-      {},
-      JSON.stringify({
-        data: [
-          {
+  module('bulkCreate', function() {
+    test('it does not interfere with normal creation', async function(assert) {
+      td.when(
+        this.postsHandler(
+          payloadMatches({ data: { type: 'posts', attributes: { title: 'First Post' } } })
+        )
+      ).thenReturn([
+        201,
+        {},
+        JSON.stringify({
+          data: {
             type: 'posts',
             id: 1,
             attributes: {
               title: 'First Post'
             }
-          },
-          {
-            type: 'posts',
-            id: 2,
-            attributes: {
-              title: 'Second Post'
-            }
           }
-        ]
-      })
-    ]);
+        })
+      ]);
 
-    const first = this.store.createRecord('post', { title: 'First Post' });
-    const second = this.store.createRecord('post', { title: 'Second Post' });
+      const first = this.store.createRecord('post', { title: 'First Post' });
 
-    const result = await this.store.bulkCreate([first, second]);
+      await first.save();
 
-    assert.equal(first.id, 1, 'The first record is updated with an ID');
-    assert.equal(second.id, 2, 'The second record is updated with an ID');
-    assert.deepEqual(result, [first, second], 'Returns the created records');
+      assert.equal(first.id, 1, 'Recieved an ID from the API');
+    });
 
-    assert.equal(
-      this.store.peekAll('post').length,
-      2,
-      'It does not add additional records to the Ember Data store'
-    );
+    test('it can create multiple models at once', async function(assert) {
+      td.when(
+        this.postsHandler(
+          payloadMatches({
+            data: [
+              { type: 'posts', attributes: { title: 'First Post' } },
+              { type: 'posts', attributes: { title: 'Second Post' } }
+            ]
+          })
+        )
+      ).thenReturn([
+        201,
+        {},
+        JSON.stringify({
+          data: [
+            {
+              type: 'posts',
+              id: 1,
+              attributes: {
+                title: 'First Post'
+              }
+            },
+            {
+              type: 'posts',
+              id: 2,
+              attributes: {
+                title: 'Second Post'
+              }
+            }
+          ]
+        })
+      ]);
+
+      const first = this.store.createRecord('post', { title: 'First Post' });
+      const second = this.store.createRecord('post', { title: 'Second Post' });
+
+      const result = await this.store.bulkCreate([first, second]);
+
+      assert.equal(first.id, 1, 'The first record is updated with an ID');
+      assert.equal(second.id, 2, 'The second record is updated with an ID');
+      assert.deepEqual(result, [first, second], 'Returns the created records');
+
+      assert.equal(
+        this.store.peekAll('post').length,
+        2,
+        'It does not add additional records to the Ember Data store'
+      );
+    });
   });
 });
