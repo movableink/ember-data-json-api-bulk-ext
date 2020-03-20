44975bf1a28b610ffaee4073c0ff7f4afe1ce128
Alex LaFroscia <alex@lafroscia.com>
2020-03-20 11:37:02 -0400

    chore(ci): match Ember Data version in LTS tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,6 +10,7 @@ module.exports = async function() {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
+            'ember-data': '~3.12.0',
             'ember-source': '~3.12.0'
           }
         }
@@ -18,6 +19,7 @@ module.exports = async function() {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
+            'ember-data': '~3.16.0',
             'ember-source': '~3.16.0'
           }
         }
